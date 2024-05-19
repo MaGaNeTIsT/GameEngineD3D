e8fa7241e8221f3938347a44a879e4df70ce38fa
MaGaNeT <yhcmaganet@gmail.com>
2024-05-19 21:07:15 +0800

    Base heap memory Ver0.1

--- Engine/Sources/EngineCore/Core/Base/Memory/Memory.h
+++ Engine/Sources/EngineCore/Core/Base/Memory/Memory.h
@@ -9,6 +9,16 @@
 namespace PigeonEngine
 {
 
+	template<typename _TResult, typename... _TArguments>
+	using TFunction = std::function<_TResult(_TArguments...)>;
+
+	template<typename _T>
+	using TUniquePtr = std::unique_ptr<_T>;
+	template<typename _T>
+	using TSharedPtr = std::shared_ptr<_T>;
+	template<typename _T>
+	using TWeakPtr = std::weak_ptr<_T>;
+
 	class EMemory final
 	{
 	public:
@@ -21,10 +31,35 @@ namespace PigeonEngine
 		{
 			return std::move<_T>(_In);
 		}
+
 		template<typename _T>
 		PE_INLINE PE_STATIC_CONSTEXPR _T&& Forward(std::remove_reference_t<_T>&& _In) noexcept
 		{
 			return std::forward<_T>(_In);
+		}
+
+		template<class _T, class... _TArguments>
+		PE_INLINE static TUniquePtr<_T> MakeUnique(_TArguments&&... _InArguments)
+		{
+			return std::make_unique(_InArguments);
+		}
+
+		template<class _T, class... _TArguments>
+		PE_INLINE PE_STATIC_CONSTEXPR TUniquePtr<_T> MakeUnique(_TArguments&&... _InArguments)
+		{
+			return std::make_unique(_InArguments);
+		}
+
+		template<class _T, typename _TSizeType = INT32>
+		PE_INLINE static TUniquePtr<_T> MakeUnique(_TSizeType _InSize)
+		{
+			return std::make_unique<_T>(_InSize);
+		}
+
+		template<class _T, typename _TSizeType = INT32>
+		PE_INLINE PE_STATIC_CONSTEXPR TUniquePtr<_T> MakeUnique(_TSizeType _InSize)
+		{
+			return std::make_unique<_T>(_InSize);
 		}
 	private:
 		EMemory()noexcept {}
@@ -41,40 +76,97 @@ namespace PigeonEngine
 		}
 	};
 
-	template<typename _T, typename _TSizeType = INT32>
+	template<typename _TSizeType = INT32>
 	class THeapBase final
 	{
 	public:
-		THeapBase()noexcept
+		PE_INLINE void Allocate(_TSizeType InSize)
 		{
-
+			if (HeapSize == InSize)
+			{
+				return;
+			}
+			Release();
+			HeapSize = InSize;
+			if (HeapSize > (_TSizeType)0)
+			{
+				HeapPtr = EMemory::MakeUnique<UINT8[]>(HeapSize);
+			}
+		}
+		PE_INLINE void Release()
+		{
+			HeapPtr = nullptr;
+			HeapSize = (_TSizeType)0;
+		}
+		PE_INLINE void Copy(const THeapBase& Other)
+		{
+			if ((Other.HeapSize > (_TSizeType)0))
+			{
+				if (HeapSize < Other.HeapSize)
+				{
+					Allocate(Other.HeapSize);
+				}
+				EMemory::Memcpy(Data(), Other.Data(), Other.HeapSize);
+			}
+		}
+		void* Data()
+		{
+			return ((void*)(HeapPtr.get()));
+		}
+		const void* Data()const
+		{
+			return ((const void*)(HeapPtr.get()));
+		}
+		_TSizeType Size()const
+		{
+			return HeapSize;
+		}
+	public:
+		THeapBase()noexcept
+			: HeapPtr(nullptr), HeapSize((_TSizeType)0)
+		{
 		}
 		THeapBase(_TSizeType InInitSize)noexcept
+			: HeapPtr(nullptr), HeapSize((_TSizeType)0)
 		{
-
+			Allocate(InInitSize);
 		}
 		~THeapBase()
 		{
-
+			Release();
 		}
 		THeapBase(const THeapBase& Other)noexcept
+			: HeapPtr(nullptr), HeapSize((_TSizeType)0)
 		{
-
+			Copy(Other);
 		}
 		THeapBase(THeapBase&& Other)noexcept
+			: HeapPtr(nullptr), HeapSize((_TSizeType)0)
 		{
-
+			HeapPtr = EMemory::Move(Other.HeapPtr);
+			HeapSize = EMemory::Move(Other.HeapSize);
 		}
 		THeapBase& operator=(const THeapBase& Other)noexcept
 		{
-
+			Release();
+			if (Other.HeapSize > (_TSizeType)0)
+			{
+				Allocate(Other.HeapSize);
+				Copy(Other);
+			}
 		}
 		THeapBase& operator=(THeapBase&& Other)noexcept
 		{
-
+			Release();
+			if (Other.HeapSize > (_TSizeType)0)
+			{
+				HeapPtr = EMemory::Move(Other.HeapPtr);
+				HeapSize = EMemory::Move(Other.HeapSize);
+			}
 		}
 	private:
-
+		TUniquePtr<UINT8[]>	HeapPtr;
+		_TSizeType			HeapSize;
 	};
 
 };
